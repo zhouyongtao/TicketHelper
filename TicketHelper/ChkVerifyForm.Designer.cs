7d63d2f9a35353d7613a7f45c69ca8b751aa40e6
irving <zhouyongtao@outlook.com>
2014-09-15 18:06:37 +0800

    mod ChkVerifyHelper

---
+++ TicketHelper/ChkVerifyForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace TicketHelper
+{
+    partial class ChkVerifyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbImg = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Verify";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SimSun", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(40, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // pbImg
+            // 
+            this.pbImg.Location = new System.Drawing.Point(34, 16);
+            this.pbImg.Name = "pbImg";
+            this.pbImg.Size = new System.Drawing.Size(172, 51);
+            this.pbImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbImg.TabIndex = 3;
+            this.pbImg.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(472, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 51);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "收集图库";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ChkVerifyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 190);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pbImg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "ChkVerifyForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ChkVerifyForm";
+            this.Load += new System.EventHandler(this.ChkVerifyForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbImg;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ TicketHelper/ChkVerifyForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TicketHelper.Ticket;
+
+namespace TicketHelper
+{
+    public partial class ChkVerifyForm : Form
+    {
+        public ChkVerifyForm()
+        {
+            InitializeComponent();
+        }
+
+        public Image img;
+        async private void ChkVerifyForm_Load(object sender, EventArgs e)
+        {
+            img = await TicketHandler.LoginInitAndGetloginVcode(IsFirst: true);
+            //初始化配置并加载验证码
+            this.pbImg.Image = img;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var bitmap = (Bitmap)img;
+
+        }
+
+        async private void button2_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 200; i++)
+            {
+                img = await TicketHandler.LoginInitAndGetloginVcode();
+                using (var bitmap = (Bitmap)img)
+                {
+                    bitmap.Save(System.Environment.CurrentDirectory + "\\verify\\img_" + i + ".png");
+                }
+            }
+        }
+    }
+}
